61c7b453dde3044237370754415ec13cc76a72ac
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-01-08 11:23:36 -0500

    Update WeekEditorState.hx

--- source/editors/WeekEditorState.hx
+++ source/editors/WeekEditorState.hx
@@ -227,7 +227,6 @@ class WeekEditorState extends MusicBeatState
 	}
 
 	var weekBeforeInputText:FlxUIInputText;
-	var difficultiesInputText:FlxUIInputText;
 	var lockedCheckbox:FlxUICheckBox;
 
 	function addLockUI() {
@@ -243,15 +242,9 @@ class WeekEditorState extends MusicBeatState
 
 		weekBeforeInputText = new FlxUIInputText(10, lockedCheckbox.y + 55, 100, '', 8);
 		blockPressWhileTypingOn.push(weekBeforeInputText);
-
-		difficultiesInputText = new FlxUIInputText(10, weekBeforeInputText.y + 60, 200, '', 8);
-		blockPressWhileTypingOn.push(difficultiesInputText);
-
+		
 		tab_group.add(new FlxText(weekBeforeInputText.x, weekBeforeInputText.y - 28, 0, 'Week File name of the Week you have\nto finish for Unlocking:'));
-		tab_group.add(new FlxText(difficultiesInputText.x, difficultiesInputText.y - 20, 0, 'Difficulties:'));
-		tab_group.add(new FlxText(difficultiesInputText.x, difficultiesInputText.y + 20, 0, 'Default difficulties are "Easy, Normal, Hard"\nwithout quotes.'));
 		tab_group.add(weekBeforeInputText);
-		tab_group.add(difficultiesInputText);
 		tab_group.add(lockedCheckbox);
 		UI_box.addGroup(tab_group);
 	}
@@ -275,10 +268,6 @@ class WeekEditorState extends MusicBeatState
 		hideCheckbox.checked = weekFile.hideStoryMode;
 
 		weekBeforeInputText.text = weekFile.weekBefore;
-
-		difficultiesInputText.text = '';
-		if(weekFile.difficulties != null) difficultiesInputText.text = weekFile.difficulties;
-
 		lockedCheckbox.checked = !weekFile.startUnlocked;
 		lock.visible = lockedCheckbox.checked;
 
@@ -400,8 +389,6 @@ class WeekEditorState extends MusicBeatState
 				updateText();
 			} else if(sender == weekBeforeInputText) {
 				weekFile.weekBefore = weekBeforeInputText.text.trim();
-			} else if(sender == difficultiesInputText) {
-				weekFile.difficulties = difficultiesInputText.text.trim();
 			}
 		}
 	}
